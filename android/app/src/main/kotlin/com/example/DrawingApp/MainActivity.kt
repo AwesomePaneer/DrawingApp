dc8e6a931cd87869f1ac4ca12e4392b9081de301
AwesomePaneer <rajnikunj2002@gmail.com>
2021-03-04 10:48:36 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/DrawingApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.DrawingApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/drawing_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DrawingHomeScreen extends StatefulWidget {
+  @override
+  _DrawingHomeScreenState createState() => _DrawingHomeScreenState();
+}
+
+class _DrawingHomeScreenState extends State<DrawingHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
